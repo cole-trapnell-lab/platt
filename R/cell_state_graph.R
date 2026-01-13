6fcc068aca5c89084dfe7d991ddaba4f6d38db90
maddyduran <duran@t003.grid.gs.washington.edu>
2026-01-12 16:19:14 -0800

    drop duplicate edges if exist

--- R/cell_state_graph.R
+++ R/cell_state_graph.R
@@ -172,27 +172,29 @@ get_graph_layout <- function(ccs,
   }
 
   edges <- edges %>% dplyr::ungroup()
-  edges <- edges %>% dplyr::select(from, to)
+  edges <- edges %>%
+    dplyr::select(from, to) %>%
+    dplyr::distinct()
   edges$weight <- 1
   edges <- edges %>% dplyr::filter(from %in% node_metadata$id & to %in% node_metadata$id)
-  
-  bidirectional_edges = find_bidirectional_edges(state_graph)
-  
+
+  bidirectional_edges <- find_bidirectional_edges(state_graph)
+
   reversed_edges <- bidirectional_edges %>%
-    dplyr::rename(temp = from) %>%
-    dplyr::mutate(from = to, to = temp) %>%
+    dplyr::rename(tmp = from) %>%
+    dplyr::mutate(from = to, to = tmp) %>%
     dplyr::select(from, to)
-  reversed_edges$edge = paste0(reversed_edges$from, reversed_edges$to)
-  
+  reversed_edges$edge <- paste0(reversed_edges$from, reversed_edges$to)
+
 
   filtered_edges <- edges %>%
     dplyr::anti_join(bidirectional_edges, by = c("from", "to"))
-  filtered_edges$edge = paste0(filtered_edges$from, filtered_edges$to)
-  
-  
-  filtered_edges$bidirectional = (filtered_edges$edge %in% reversed_edges$edge)
-  filtered_edges$edge = NULL
-  
+  filtered_edges$edge <- paste0(filtered_edges$from, filtered_edges$to)
+
+
+  filtered_edges$bidirectional <- (filtered_edges$edge %in% reversed_edges$edge)
+  filtered_edges$edge <- NULL
+
   G <- filtered_edges %>%
     dplyr::distinct() %>%
     igraph::graph_from_data_frame(directed = TRUE, vertices = node_metadata)
@@ -270,20 +272,20 @@ find_bidirectional_edges <- function(graph) {
   if (!igraph::is.directed(graph)) {
     stop("Graph must be directed to find bidirectional edges.")
   }
-  
-  edges <- igraph::as_data_frame(graph, what = "edges")
-  
+
+  edges <- igraph::as_data_frame(graph, what = "edges") %>%
+    dplyr::distinct(from, to)
+
   # Create a pair key to identify reverse duplicates
   edges$pair_key <- paste(pmin(edges$from, edges$to), pmax(edges$from, edges$to), sep = "_")
-  
+
   # Find keys that occur twice (i.e., bidirectional)
   bidir_keys <- edges %>%
     dplyr::group_by(pair_key) %>%
     dplyr::filter(n() == 2) %>%
-    dplyr::slice(1) %>%  # Keep only one of the two directions
+    dplyr::slice(1) %>% # Keep only one of the two directions
     dplyr::ungroup() %>%
     dplyr::select(from, to)
-  
+
   return(bidir_keys)
 }
-
