84e750e13503264fc79794089e9a724c0901dd38
Eliza Barkan <elizabarkan@Fresh-MacBook-Pro-KJ31219GNV.local>
2026-04-08 11:04:32 -0700

    Revert "move in plot figure legends to bottom for interactive plots"
    This reverts commit 1acb1ad8dbc9592e35f9e3afb81c1beda2cf68f7.

--- R/plot_phenotypes.R
+++ R/plot_phenotypes.R
@@ -954,10 +954,6 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
             ggplot2::scale_shape_manual(
                 values = c("Observed phenotype" = 21, "Expected phenotype" = 22),
                 name = "Shape",
-                labels = c(
-                    "Observed phenotype" = "Effect not expected",
-                    "Expected phenotype" = "Effect expected"
-                ),
                 guide = ggplot2::guide_legend(order = 4)
             )
     } else {
@@ -965,10 +961,6 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
             ggplot2::scale_shape_manual(
                 values = c("Observed phenotype" = 21, "Expected phenotype" = 22),
                 name = "Shape",
-                labels = c(
-                    "Observed phenotype" = "Effect not expected",
-                    "Expected phenotype" = "Effect expected"
-                ),
                 guide = ggplot2::guide_legend(order = 4)
             ) + guides(shape = "none")
     }
@@ -1070,7 +1062,6 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
                 aes(x = x, y = y, label = glyph, color = glyph_type),
                 inherit.aes = FALSE,
                 fontface = "bold",
-                key_glyph = ggplot2::draw_key_text,
                 show.legend = c(color = TRUE)
             ) +
             scale_color_manual(
@@ -1089,8 +1080,7 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
                     override.aes = list(
                         label = c("", "<<", ">>", "!!", "<>", "##"),
                         size = 3,
-                        colour = "black",
-                        alpha = 1
+                        colour = "black"
                     ),
                     order = 6
                 )
@@ -1119,15 +1109,11 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
     legend_text_size <- if (identical(render_mode, "global")) 11 else 8.5
     legend_key_size <- if (identical(render_mode, "global")) grid::unit(15, "pt") else grid::unit(10, "pt")
 
-    if (isTRUE(interactive) && !identical(legend_position, "none")) {
+    if (isTRUE(interactive) && identical(render_mode, "global") && !identical(legend_position, "none")) {
         legend_position_final <- "bottom"
         legend_box_final <- "horizontal"
         legend_direction_final <- "vertical"
-        if (identical(render_mode, "global")) {
-            legend_title_size <- legend_title_size * 3
-            legend_text_size <- legend_text_size * 3
-            legend_key_size <- grid::unit(54, "pt")
-        }
+        legend_key_size <- grid::unit(18, "pt")
     }
 
     legend_theme <- ggplot2::theme(
