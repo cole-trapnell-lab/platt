208ead5bc9413a4a123e7376d486bb0f9dcf471c
Eliza Barkan <elizabarkan@Fresh-MacBook-Pro-KJ31219GNV.local>
2026-04-10 12:14:48 -0700

    Revert "add back autonomy legend, add gray node to tissue specific legend"
    This reverts commit 7185934715e38ec31f7fb0a44092f9849ea0d984.

--- R/plot_phenotypes.R
+++ R/plot_phenotypes.R
@@ -882,9 +882,9 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
             ggplot2::aes(x = x, y = y, fill = severity_fill),
             inherit.aes = FALSE,
             shape = 21,
-            size = 0.01,
-            alpha = 1,
-            colour = "black",
+            size = 0,
+            alpha = 0,
+            colour = NA,
             stroke = 0,
             show.legend = c(fill = TRUE)
         ) +
@@ -911,6 +911,13 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
                 override.aes = list(
                     shape = 21,
                     size = 7,
+                    fill = c(
+                        unname(phenotype_colors["abundance_loss"]),
+                        unname(phenotype_colors["identity"]),
+                        unname(phenotype_colors["abundance_gain"]),
+                        unname(phenotype_colors["none"]),
+                        unname(phenotype_colors["not_present"])
+                    ),
                     colour = "black",
                     stroke = 0.5,
                     alpha = 1
@@ -1033,10 +1040,11 @@ plot_phenotypes_glyphs <- function(cell_state_graph,
             ggplot2::aes(x = x, y = y, alpha = autonomy_display),
             inherit.aes = FALSE,
             shape = 21,
-            size = 0.01,
-            fill = "black",
-            colour = "black",
+            size = 0,
+            fill = NA,
+            colour = NA,
             stroke = 0,
+            alpha = 0,
             show.legend = c(alpha = TRUE)
         ) +
         ggplot2::scale_alpha_manual(
